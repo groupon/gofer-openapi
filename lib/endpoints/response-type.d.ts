880ba2af9f4f3f523575273067ee476390b50738
David Bushong <dbushong@groupon.com>
2021-11-02 19:25:40 -0700

    feat: support non-json response types

--- lib/endpoints/response-type.d.ts
+++ lib/endpoints/response-type.d.ts
@@ -1,3 +1,7 @@
 import * as t from '@babel/types';
 import type { OpenAPIV3 as o } from 'openapi-types';
-export default function buildResponseType(responses: o.ResponsesObject, components: o.ComponentsObject): t.GenericTypeAnnotation;
+export default function buildResponseType(responses: o.ResponsesObject, components: o.ComponentsObject): {
+    goferMethod: string;
+    responseType: t.FlowType;
+    acceptMimeType?: string;
+};
--- src/endpoints/response-type.ts
+++ src/endpoints/response-type.ts
@@ -30,18 +30,41 @@
  */
 import * as t from '@babel/types';
 import type { OpenAPIV3 as o } from 'openapi-types';
+import Debug from 'debug';
+
 import { resolveRef } from '../refs';
 import { schemaToAnnotation } from '../schema';
+
+const debug = Debug('gofer:openapi:response-type');
 
 export default function buildResponseType(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   responses: o.ResponsesObject,
   components: o.ComponentsObject
-) {
-  // if no response declaration, return Promise<void>
-  let typeAnn: t.FlowType = t.voidTypeAnnotation();
-
+): { goferMethod: string; responseType: t.FlowType; acceptMimeType?: string } {
   // TODO: handle error statuses too
+
+  // Right now, for success, there's a few possibilities, in decreasing order
+  // of preference:
+  //
+  // 1. application/json exists:
+  //    * call .json()
+  //    * have return type be based on .content.schema if possible, else "any"
+  // 2. one of our known text types exists (xml, yaml, etc)
+  //    * call .text()
+  //    * send { headers: { Accept: <that type> } }
+  //    * return type is "string"
+  // 3. any other unknown type(s) exist
+  //    * call .rawBody()
+  //    * return type is Buffer
+  // 4. we specifically have a no-content response code (201, 204, etc)
+  //    * call .rawBody() to resolve the output promise
+  //    * return type is unknown (should be void, but that's more annoying)
+  // 5. nothing exists - assume a crummy schema and try to be as helpful as
+  //    possible
+  //    * call .json()
+  //    * return type is "any"
+
   const refOrResp = responses[200];
   if (refOrResp) {
     let resp: o.ResponseObject;
@@ -50,12 +73,69 @@ export default function buildResponseType(
     } else {
       resp = refOrResp;
     }
-    const respSchema = resp.content?.['application/json']?.schema;
-    if (respSchema) typeAnn = schemaToAnnotation(respSchema);
+    const content = resp.content || {};
+    let textType: string | null;
+    if ('application/json' in content) {
+      const respSchema = content['application/json']?.schema;
+      // (1)
+      return {
+        goferMethod: 'json',
+        responseType: respSchema
+          ? schemaToAnnotation(respSchema, ['application/json'])
+          : t.anyTypeAnnotation(),
+      };
+    } else if ((textType = findTextType(content))) {
+      // (2)
+      debug('Found text mime type %s', textType);
+      return {
+        goferMethod: 'text',
+        responseType: t.stringTypeAnnotation(),
+        // if there's only one type specified, no need for Accept header
+        acceptMimeType: Object.keys(content).length > 1 ? textType : undefined,
+      };
+    } else if (Object.keys(content).length > 0) {
+      // (3)
+      debug(
+        'Found unknown mime type(s); returning Buffer: ',
+        Object.keys(content)
+      );
+      return {
+        goferMethod: 'rawBody',
+        responseType: t.genericTypeAnnotation(t.identifier('Buffer')),
+      };
+    }
+  } else if (
+    responses[201] ||
+    responses[202] ||
+    responses[203] ||
+    responses[204]
+  ) {
+    // (4)
+    debug('Found no-content status code; returning unknown');
+    return {
+      goferMethod: 'rawBody',
+      responseType: t.genericTypeAnnotation(t.identifier('unknown')),
+    };
   }
 
-  return t.genericTypeAnnotation(
-    t.identifier('Promise'),
-    t.typeParameterInstantiation([typeAnn])
-  );
+  // (5)
+  debug('Found no good responses.200.content; returning any');
+  return { goferMethod: 'json', responseType: t.anyTypeAnnotation() };
+}
+
+/**
+ * returns the most preferred mime type, if found
+ */
+function findTextType(content: Record<string, unknown>): string | null {
+  for (const mt of [
+    'text/yaml',
+    'application/yaml',
+    'text/xml',
+    'application/xml',
+    'text/plain',
+    'text/html',
+  ]) {
+    if (mt in content) return mt;
+  }
+  return null;
 }
